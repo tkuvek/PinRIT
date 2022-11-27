43de2026262507bf58813515d3008cf480c2ab13
tkuvek <kuvektomislav@gmail.com>
2022-11-27 12:12:25 +0100

    Change direct import to relative in contract, change .env location

--- blockchain_testing/contracts/PinRIT.sol
+++ blockchain_testing/contracts/PinRIT.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Base64.sol";
 
 contract PinRIT is ERC721URIStorage {
     using Strings for uint256;
