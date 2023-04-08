493d943de6a555b738317b9dc540696954906a15
tkuvek <kuvektomislav@gmail.com>
2023-04-09 00:11:52 +0200

    Remove sslmode to enable Heroku and CockroachDB connection

--- utils/db_conn.py
+++ utils/db_conn.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 Session = sessionmaker()
 
 def connect_db():
-    engine = create_engine("cockroachdb://pinRIT:2HMcKbOxiTQFR0tkzoLNbQ@sneaky-shrimp-6916.7tc.cockroachlabs.cloud:26257/pinrit_db?sslmode=verify-full")
+    engine = create_engine("cockroachdb://pinRIT:2HMcKbOxiTQFR0tkzoLNbQ@sneaky-shrimp-6916.7tc.cockroachlabs.cloud:26257/pinrit_db")
     conn = engine.connect()
 
     # res = conn.execute(text("SELECT now()")).fetchall()
